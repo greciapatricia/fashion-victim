acdc7e4306d8f559f245a5aa989c968747b7b466
Grecia Patricia <grecia.busquets@gmail.com>
2021-05-19 20:48:18 +0200

    uploading content

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    const game = new Game('game');
+  
+    document.getElementById("start-button").onclick = () => {
+      game.startGame()
+    };
+  
+    /*document.addEventListener("keydown", () => {
+      game.onKeyEvent(event);
+    });
+  
+    document.addEventListener("keyup", () => {
+      game.onKeyEvent(event);
+    });*/
+  });
